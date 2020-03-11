f96b11f8fd365d658d300ccb36f3b8a2d60d96d2
bumbot <pkim1295@gmail.com>
2020-03-11 11:51:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(j => renderBooks(j))
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
+  debugger
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
