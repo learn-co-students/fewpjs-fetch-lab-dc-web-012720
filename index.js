48c347021b9fcfe6832c61c9d19179b03056ce0e
alanbanks229 <alanbanks229@gmail.com>
2020-03-11 12:26:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function fetchBooks() {
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+  console.log("hello there")
+})
 
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+
+    .then(resp => resp.json()) //returns resp.json and pass it as ===> function(json) or json => console.log(json)
+    .then(json => renderBooks(json))
+
+    //Commented out code below works as well... for bigger picture...
+
+    // .then(function(response) {  //=> .then(response => response.json())
+    //   return response.json();
+    // })
+    // .then(function(json){
+    //   renderBooks(json)
+    //   //Use data inside json to do DOM manipulation
+    // })
 }
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
@@ -11,6 +30,11 @@ function renderBooks(json) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then(function(response) {  //=> .then(response => response.json())
+//     return response.json();
+//   })
+//   .then(function(json){
+//     console.log(json)
+//     //Use data inside json to do DOM manipulation
+//   })
